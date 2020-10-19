3cfd5eeb4da478bd4caea1eb7f42d09bfde007f3
Chi Ahrens <chi_son@yahoo.com>
2020-10-19 13:24:30 -0700

    Update super_simple.py

--- super_simple.py
+++ super_simple.py
@@ -2,10 +2,10 @@ from spike import Motor, MotorPair, ColorSensor, PrimeHub
 import time
 
 # Define MotorPair with right motor on port A and left motor on port B. Set wheel size to 11 inches and default speed to 45%.
-DEFAULT_SPEED=35
+WHEEL_CIRCUMFERENCE = 11
 motor_pair = MotorPair('A', 'E')
-motor_pair.set_motor_rotation(11, 'in')
-motor_pair.set_default_speed(DEFAULT_SPEED)
+motor_pair.set_motor_rotation(WHEEL_CIRCUMFERENCE, 'in')
+motor_pair.set_default_speed(35)
 left_motor= Motor('A')
 right_motor = Motor('E')
 front = Motor('D')
@@ -13,40 +13,48 @@ back = Motor('C')
 left_light = ColorSensor('B')
 right_light = ColorSensor('F')
 
+def reset_arms():
+    front.start(50)
+    back.start(50)
+    time.sleep(.5)
+    front.stop()
+    back.stop()
+
+# Reset odometer to 0
+def reset_odometer():
+    right_motor.set_degrees_counted(0)
+
+# Read odometer
+def read_odometer():
+    return (right_motor.get_degrees_counted() / 360) * WHEEL_CIRCUMFERENCE
+
+# Get reflected light from light sensor. If side is 'left', use left sensor. If side is 'right', use right sensor
+def get_reflected_light(side='left'):
+    if side == 'left':
+        return left_light.get_reflected_light() - 50
+    else:
+        return right_light.get_reflected_light() - 50
+
 # Rotates the robot by specified angle. Positive to rotate right. Negative to rotate left.
 def rotate(angle):
     motor_pair.move(angle * 1.67, 'degrees', 100, 30)
 
 # Moves robot forward or backward by inches. If angle is set, it rotates the robot by specified angle first.
-def move(inches, angle=0, speed=None):
-    if angle != 0:
-        rotate(angle)
-    motor_pair.move(inches, 'in', speed=speed)
+def move(inches, steering=0, speed=None):
+    motor_pair.move(inches, 'in', steering, speed=speed)
 
-def follow_line_left(inches, speed=DEFAULT_SPEED):
-    right_motor.set_degrees_counted(0)
-    while right_motor.get_degrees_counted() * 11 / 360 < inches:
-        steering = int(left_light.get_reflected_light() - 50)
+def follow_line(inches, side='left', speed=motor_pair.get_default_speed()):
+    reset_odometer()
+    while read_odometer() < inches:
+        steering = int(get_reflected_light(side))
         motor_pair.start_at_power(speed, steering)
     motor_pair.stop()
 
-def follow_line_right(inches, speed=DEFAULT_SPEED):
-    right_motor.set_degrees_counted(0)
-    while right_motor.get_degrees_counted() * 11 / 360 < inches:
-        steering = int(right_light.get_reflected_light() - 50)
-        motor_pair.start_at_power(speed, steering)
-    motor_pair.stop()
+def front_arm(degrees, speed=25):
+    front.run_for_degrees(degrees * 2, speed)
 
-def front_arm(seconds, speed):
-    front.start(speed)
-    time.sleep(seconds)
-    front.stop()
-
-
-def back_arm(seconds, speed):
-    back.start(speed)
-    time.sleep(seconds)
-    back.stop()
+def back_arm(degrees, speed=25):
+    back.run_for_degrees(degrees * 2, speed)
 
 def m02():
     move(-27)
@@ -56,41 +64,43 @@ def m02():
     back_arm(1, 25)
 
 def m11():
-    move(3, -25)
+    rotate(-25)
+    move(3)
     follow_line_right(50)
-    back_arm(1, -22)
-    move(-15, 175)
+    back_arm(-45)
+    rotate(175)
+    move(-15)
     left_motor.run_for_seconds(2)
     move(10)
-    back_arm(1, 25)
-    move(55, 0)
+    back_arm(45)
+    move(55)
 
 def m13():
-    front_arm(2, -20)
-    move(3, -25)
+    reset_arms()
+    front_arm(-90)
+    rotate(-25)
+    move(3)
     follow_line_right(29)
     rotate(-90)
     follow_line_right(12)
     move(6)
     rotate(80)
     follow_line_right(22.5)
-    front_arm(2, 20)
-    move(2, -90)
-    back_arm(1, -20)
-    front_arm(2, -20)
-    front_arm(2, 20)
-    back_arm(1, 20)
+    front_arm(90)
+    rotate(-90)
+    move(2)
+    back_arm(-90)
+    front_arm(-90)
+    reset_arms()
     move(-5)
-    move(40, -90)
-    move(28, -45)
+    rotate(-90)
+    move(40)
+    rotate(-45)
+    move(28)
 
 #m02()
-m11()
+#m11()
 #m13()
-
-#front_arm(2, -20)
-#front_arm(2, 20)
-#back_arm(1, 20)
 
 # Exit code
 raise SystemExit()
