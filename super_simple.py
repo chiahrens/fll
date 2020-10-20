ccd4f1b29c04db95d1f36253c7ba54478eb97910
Chi Ahrens <chi_son@yahoo.com>
2020-10-19 20:02:04 -0700

    Update super_simple.py

--- super_simple.py
+++ super_simple.py
@@ -10,8 +10,8 @@ left_motor= Motor('A')
 right_motor = Motor('E')
 front = Motor('D')
 back = Motor('C')
-left_light = ColorSensor('B')
-right_light = ColorSensor('F')
+left = ColorSensor('B')
+right = ColorSensor('F')
 
 def reset_arms():
     front.start(50)
@@ -28,13 +28,6 @@ def reset_odometer():
 def read_odometer():
     return (right_motor.get_degrees_counted() / 360) * WHEEL_CIRCUMFERENCE
 
-# Get reflected light from light sensor. If side is 'left', use left sensor. If side is 'right', use right sensor
-def get_reflected_light(side='left'):
-    if side == 'left':
-        return left_light.get_reflected_light() - 50
-    else:
-        return right_light.get_reflected_light() - 50
-
 # Rotates the robot by specified angle. Positive to rotate right. Negative to rotate left.
 def rotate(angle):
     motor_pair.move(angle * 1.67, 'degrees', 100, 30)
@@ -43,29 +36,27 @@ def rotate(angle):
 def move(inches, steering=0, speed=None):
     motor_pair.move(inches, 'in', steering, speed=speed)
 
-def follow_line(inches, side='left', speed=motor_pair.get_default_speed()):
+def follow_line(side, inches, speed=motor_pair.get_default_speed()):
     reset_odometer()
     while read_odometer() < inches:
-        steering = int(get_reflected_light(side))
+        steering = int(side.get_reflected_light() - 50)
         motor_pair.start_at_power(speed, steering)
     motor_pair.stop()
 
-def front_arm(degrees, speed=25):
-    front.run_for_degrees(degrees * 2, speed)
-
-def front_arm2(degrees, speed=25):
-    front.set_degrees_counted(0)
-    while abs(front.get_degrees_counted()) < abs(degrees * 2):
-        front.start(speed)
-    front.stop()
-
-def back_arm(degrees, speed=25):
-    back.run_for_degrees(degrees * 2, speed)
+def arm(motor, degrees, speed=25):
+    motor.set_degrees_counted(0)
+    if(degrees > 0):
+        motor.start(speed)
+    elif(degrees < 0):
+        motor.start(-speed)
+    while abs(motor.get_degrees_counted()) < abs(degrees * 2):
+        continue
+    motor.stop()
 
 def m02():
     reset_arms()
     move(-26.5)
-    back_arm(-55)
+    arm(back, -55)
     move(-10, speed=5)
     move(35)
 
@@ -73,8 +64,8 @@ def m11():
     reset_arms()
     rotate(-25)
     move(3)
-    follow_line(50, 'right')
-    back_arm(-55)
+    follow_line(right, 50)
+    arm(back, -55)
     rotate(175)
     move(-15)
     left_motor.run_for_seconds(2)
@@ -82,20 +73,20 @@ def m11():
 
 def m13():
     reset_arms()
-    front_arm(-90)
+    arm(front, -90)
     rotate(-25)
     move(3)
-    follow_line(29, 'right')
+    follow_line(right, 29)
     rotate(-90)
-    follow_line(12, 'right')
+    follow_line(right, 12')
     move(6)
     rotate(80)
-    follow_line(22, 'right')
-    front_arm(90)
+    follow_line(right, 22)
+    arm(front, 90)
     rotate(-88)
     move(1)
-    back_arm(-90)
-    front_arm(-90, speed=100)
+    arm(back, -90)
+    arm(front, -90, speed=100)
     reset_arms()
     move(-3)
     rotate(-90)
@@ -107,23 +98,23 @@ def m08():
     reset_arms()
     rotate(-45)
     move(6.5)
-    follow_line(9, 'right')
+    follow_line(right, 9)
     move(3)
     rotate(-50)
-    front_arm(45, -25)
-    follow_line(18, 'right')
+    arm(front, 45, -25)
+    follow_line(right, 18)
     rotate(-5)
-    front_arm(45, 25)
+    arm(front, 45, 25)
 
 def m05():
     reset_arms()
     rotate(-45)
     move(6.5)
-    follow_line(9, 'right')
+    follow_line(right, 9)
     move(3)
     rotate(-50)
-    front_arm(90, -25)
-    follow_line(17.7, 'right')
+    arm(front, 90, -25)
+    follow_line(right, 17.7)
     rotate(-5)
     front.run_for_seconds(1, 50)
     rotate(-45)
@@ -138,9 +129,11 @@ def m05():
 #m11()
 #m13()
 #m08()
-m05()
+#m05()
 
-#front_arm(45, 100)
+reset_arms()
+arm(front, -90)
+arm(back, -90)
 
 # Exit code
 raise SystemExit()
