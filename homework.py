578c2c9b5ded380f0b659557a059e9c08477d45a
Chi Ahrens <chi_son@yahoo.com>
2020-10-23 09:06:58 -0700

    Update homework.py

--- homework.py
+++ homework.py
@@ -13,6 +13,16 @@ color_sensor = ColorSensor('F')
 
 
 # Homework assignment #2
+# Write a function to rotate the robot by exact degrees
+# The current function will cause the robot to turn either right (postive), or left (negative), but not the correct amount
+# Example:
+#   rotate(90) should make the robot rotate 90 degrees to the right
+#   rotate(-90) should make the robot rotate 90 degrees to the left
+def rotate(degrees):
+  motor_pair.move(degrees * 1, 'degrees', 100, 20)
+
+  
+# Homework assignment #3
 # Write a line follower function that stops when the wheel rotation reaches the "amount" parameter
 # The current logic is from the example and never stops. Change it so that it does
 # Hint #1: right_wheel.set_degrees_counted(0) resets the right wheel rotation counter to zero
@@ -27,24 +37,16 @@ def follow_line_rotation(amount):
     motor_pair.start_at_power(35, int(steering))
   motor_pair.stop()
   
-# Homework assignment #3
-# Write a function to rotate the robot by exact degrees
-# The current function will cause the robot to turn either right (postive), or left (negative), but not the correct amount
-# Example:
-#   rotate(90) should make the robot rotate 90 degrees to the right
-#   rotate(-90) should make the robot rotate 90 degrees to the left
-def rotate(degrees):
-  motor_pair.move(degrees * 1, 'degrees', 100, 20)
-  
+
 # Homework assignment #4 Optional
-# Change follow_line from assignment #2 to stop at amount in cm
+# Change follow_line from assignment #3 to stop at amount in cm
 # Hint #1: There are 360 degrees in a wheel rotation. The circumference of the wheel is 27.63 cm.
 # Example:
 #   follow_line_cm(100) should make the robot follow the line for 100 cm
 def follow_line_cm(amount):
   
 # Homework assignment #5 Super Optional
-# Change follow_line from assignment #5 to stop at the 
+# Change follow_line from assignment #4 to stop at the 
 #   next perpendicular black line after the amount in cm has been reached
 # Example:
 #   follow_line(100) should make the robot follow the line for 100 cm,
