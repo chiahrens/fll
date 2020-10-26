f3113e2186d49400d36f07f999fd3f026ad77ac0
Chi Ahrens <chi_son@yahoo.com>
2020-10-26 08:39:03 -0700

    Update nav2.py

--- nav2.py
+++ nav2.py
@@ -2,126 +2,115 @@ from spike import Motor, MotorPair, ColorSensor, PrimeHub
 import time
 
 # Initialize motors and sensors
-WHEEL_CIRCUMFERENCE = 11
-DEFAULT_SPEED = 30
+WHEEL_CIRCUMFERENCE_CM = 27.63
+DEFAULT_SPEED = 35
 robot = MotorPair('A', 'E')
-robot.set_motor_rotation(WHEEL_CIRCUMFERENCE, 'in')
+robot.set_motor_rotation(WHEEL_CIRCUMFERENCE_CM)
 robot.set_default_speed(DEFAULT_SPEED)
-left_wheel= Motor('A')
 right_wheel = Motor('E')
-front = Motor('D')
-back = Motor('C')
 left = ColorSensor('B')
 right = ColorSensor('F')
 
-
-# Move front or back attachment
-# Example:
-#arm(front, 45)        # Move the front attachment up by 45 degrees
-#arm(back, -90)        # Move the back attachment down by 90 degrees
-def arm(motor, degrees, speed=25):
-    # motor.run_for_rotation(rotations, speed)
-    motor.set_degrees_counted(0)
-    if(degrees > 0):
-        motor.start(speed)
-    elif(degrees < 0):
-        motor.start(-speed)
-    while abs(motor.get_degrees_counted()) < abs(degrees * 2):
-        continue
-    motor.stop()
-
-# Set front and back arms in up postions
-def reset_arms(front_degrees=0, back_degrees=0):
-    front.start(50)
-    back.start(50)
-    time.sleep(.5)
-    front.stop()
-    back.stop()
-    arm(front, front_degrees)
-    arm(back, back_degrees)
 
 # Reset odometer to 0
 def reset_odometer():
     right_wheel.set_degrees_counted(0)
 
-# Read odometer. Return distance traveled in inches
+
+# Read odometer. Return distance traveled in cm
 def read_odometer():
-    return (right_wheel.get_degrees_counted() / 360) * WHEEL_CIRCUMFERENCE
+    return (right_wheel.get_degrees_counted() / 360) * WHEEL_CIRCUMFERENCE_CM
 
-def turn(angle, steering, turn_multiplier, speed=20):
-    if angle > 0:
-        start = left_wheel.get_degrees_counted()
-        robot.start(steering, speed)
-        while abs(left_wheel.get_degrees_counted() - start) < abs(angle * turn_multiplier) :
-            continue
-    elif angle < 0:
-        start = right_wheel.get_degrees_counted()
-        robot.start(-steering, speed)
-        while abs(right_wheel.get_degrees_counted() - start) < abs(angle * turn_multiplier) :
-            continue
-    
+
+# Rotates the robot by specified angle - positive to rotate right, negative to rotate left.
+# Parameters:
+#angle - Required. Degrees to rotate. Positive to rotate right. Negative to rotate left.
+#speed - Default 20. Rotation speed.
+# Example:
+#rotate(90)        # Rotates the robot 90 degrees to the right
+#rotate(-180)    # Rotates the robot 180 degrees to the left
 def rotate(angle, speed=20):
-    turn(angle, 100, 1.6, speed)
+    robot.move(angle * 1.67, 'degrees', 100, speed)
 
-def pivot(angle, speed=20):
-    turn(angle, 99, 3.2, speed)
 
-def move(inches, steering=0, speed=DEFAULT_SPEED):
+# Follow line for specified number of cm.
+# Parameters:
+#cm - Required. Number of cm the robot should follow the line
+#line_stop - Default False. If set to True, it will continue until next stop line.
+#steer_x - Default 1. Setting this higher will cause sharper turns.
+#speed - Default DEFAULT_SPEED. Speed of the robot.
+# Example:
+#follow_line(15)                # Follow line for 15 cm and stop
+#follow_line(15, True)        # Follow line for 15 cm, but stop at next stop line
+#follow_line(15, False, 2, 10)# Follow line for 15 cm, at 10% speed with sharper turns
+def follow_line(cm, line_stop=False, steer_x=1, speed=DEFAULT_SPEED):
+    reset_odometer()
+    while read_odometer() < cm or (line_stop == True and left.get_reflected_light() > 25):
+        steering = int(right.get_reflected_light() - 50) * steer_x
+        robot.start_at_power(speed, steering)
+    if line_stop == True:
+        robot.stop()
+
+def move(cm, steering=0, speed=None)
     reset_odometer()
     robot.start(steering, speed)
-    while read_odometer() < inches:
+    while read_odometer() < cm:
         continue
-
-def follow_line(inches, speed=DEFAULT_SPEED):
-    reset_odometer()
-    while read_odometer() < inches:
-        steering = int(right.get_reflected_light() - 50)
-        robot.start_at_power(speed, steering)
-
-def stop_at_line(speed=DEFAULT_SPEED):
-    while left.get_reflected_light() > 25:
-        steering = int(right.get_reflected_light() - 50)
-        robot.start_at_power(speed, steering)
-    robot.stop()
-
 
 ################################ Helper functions Start Here ################################
 
-def start_top_line(inches):
-    #reset_arms()
-    rotate(-26)
-    move(6.6)
+def bottom_horizontal(cm):
+    move(23, -3)
+    follow_line(cm, True)
+
+def left_vertical(cm):
+    move(19, -46)
+    follow_line(20)
+    follow_line(15, False, 2, 15)
+    follow_line(cm, True)
+
+def top_horizontal(cm):
+    move(19, -46)
+    follow_line(20)
+    follow_line(15, False, 2, 15)
+    move(8, 30)
+    follow_line(cm, True)
+
+################################ Mission functions Start Here ################################
+
+def m06:
+    bottom_horizontal(64)
+
+def m09:
+    bottom_horizontal(90)
+
+def m11:
+    bottom_horizontal(121)
+
+def m04:
+    left_vertical(18)
+
+def m08:
+    left_vertical(42)
+
+def m07:
+    top_horizontal(29)
+
+def m08:
+    top_horizontal(45)
+
+def m10:
+    top_horizontal(70)
+
+def m13:
+    top_horizontal(103)
+
+def m06_08:
+    bottom_horizontal(64)
+    robot.move(6)
+    rotate(-90)
+    follow_line(30)
+    robot.move(7)
     follow_line(10)
-    robot.stop()
-    pivot(-45)
-    follow_line(inches, True)
-    stop_at_line()
+    robot.move(8)
 
-def start_bottom_line(inches):
-    #reset_arms()
-    move(8.3)
-    follow_line(inches, True)
-    stop_at_line()
-
-def start_line3(inches):
-    #reset_arms()
-    rotate(-26)
-    move(6.6)
-    line(10)
-    robot.stop()
-    rotate(-25)
-    move(13.5)
-    follow_line(inches, True)
-    stop_at_line()
-
-#start_bottom_line(35)
-#start_bottom_line(45)
-#start_bottom_line(58)
-
-#start_top_line(29.5)
-#start_top_line(37)
-
-#start_line3(36)
-#start_line3(42)
-#start_line3(52)
-#start_line3(64)
